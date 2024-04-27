0e920875aff5dfebd4d8fd1bdfc10f6de5036aa1
chahyunsoo <chahs1012@naver.com>
2024-04-27 18:08:24 +0900

    Initial Project

---
+++ src/test/java/backend/univfit/UnivfitApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.univfit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnivfitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

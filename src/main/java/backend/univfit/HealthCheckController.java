76e46dfd528eb54b91ad8eb71f0cd4b9e1741eb0
chahyunsoo <chahs1012@naver.com>
2024-05-04 14:21:00 +0900

    [feat] : HealthCheckController 추가

---
+++ src/main/java/backend/univfit/HealthCheckController.java
@@ -0,0 +1,14 @@
+package backend.univfit;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+    @GetMapping("/health-check")
+    public String hello() {
+        return "Hello, UnivFit!";
+    }
+
+}
+

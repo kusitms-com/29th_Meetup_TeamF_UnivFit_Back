aaae9d5e0c365c542b3046dbb71267e535afaa7f
김영록 <praoo800@konkuk.ac.kr>
2024-05-18 01:25:51 +0900

    refactor/applyStatus필드 추가

---
+++ src/main/java/backend/univfit/domain/apply/entity/enums/ApplyStatus.java
@@ -0,0 +1,11 @@
+package backend.univfit.domain.apply.entity.enums;
+
+/**
+ * NOT_YET : 지원예정
+ * APPLY : 지원완료
+ * PASS : 합격
+ * FAIL : 불합격
+ */
+public enum ApplyStatus {
+    NOT_YET, APPLY, PASS, FAIL
+}

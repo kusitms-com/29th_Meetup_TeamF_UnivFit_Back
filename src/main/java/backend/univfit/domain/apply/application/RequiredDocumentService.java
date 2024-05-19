9050ad87d61c1c16664f1ad65006fe6c190d09b3
chahyunsoo <chahs1012@naver.com>
2024-05-19 16:37:40 +0900

    [fix] : 필요 서류 상세 조회 시 사용자 식별문제 해결

--- src/main/java/backend/univfit/domain/apply/application/RequiredDocumentService.java
+++ src/main/java/backend/univfit/domain/apply/application/RequiredDocumentService.java
@@ -1,11 +1,12 @@
 package backend.univfit.domain.apply.application;
 
 import backend.univfit.domain.apply.api.dto.response.RequiredDocumentResponse;
+import backend.univfit.global.argumentResolver.MemberInfoObject;
 
 import java.util.List;
 
 public interface RequiredDocumentService {
-    List<RequiredDocumentResponse> getRequiredDocumentList(Long announcementId);
+    List<RequiredDocumentResponse> getRequiredDocumentList(Long announcementId, MemberInfoObject memberInfoObject);
 
     RequiredDocumentResponse getRequiredDocument(Long announcementId, Long requiredDocumentId);
 
